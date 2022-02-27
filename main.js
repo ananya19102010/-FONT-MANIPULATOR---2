f6ae1b5ca1b7509d896525e6e6a3b14449c707a7
ananya19102010 <73178449+ananya19102010@users.noreply.github.com>
2022-02-27 10:25:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    background('#95A1AB');
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+   if(results.length>0){
+    console.log(results);
+   }
+}
